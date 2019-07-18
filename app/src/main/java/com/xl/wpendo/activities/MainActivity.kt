a76d449eb78d068eeac748c8720174245c85f947
15734lemon <844742916@qq.com>
2019-07-18 22:28:31 +0800

    basic richtext

--- app/src/main/java/com/xl/wpendo/activities/MainActivity.kt
+++ app/src/main/java/com/xl/wpendo/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.xl.wpendo.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.xl.wpendo.R
 
 class MainActivity : BaseActivity() {
@@ -9,5 +10,6 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/xl/wpendo/activities/chenyang/myenums/typeface.kt
@@ -0,0 +1,9 @@
+package com.xl.wpendo.activities.chenyang.myenums
+
+/**
+ *created by chenyang
+ *on 2019/7/18
+ */
+enum class typeface{
+
+}
